72c329b636cf0e3131dea66905d403825859079e
Dilip965 <nandiwaledilip@gmail.com>
2024-12-16 08:12:19 +0530

    it is nice

---
+++ test.js
@@ -0,0 +1,49 @@
+console.log("Hello world")
+//it is has syntax
+//colour coding can be change
+//how can you execute it 
+//softawre
+//compiler havs syntax 
+//.js ,accept and do later depend on syntax
+/*
+index.html
+index.js
+index.css
+previously it was
+people not used like this
+that only used there script
+that is attached with browser 
+javascpirt stand alone excute you can do it browser
+it is using in backent ,mobile much more 
+
+install node.js
+it is just file download 
+write we don't do on google 
+you can do it file from 
+broser also you
+Node js
+deno js
+are same founder
+ryan Dahl
+Networking also help
+boss
+Ryan Dahl is the creator of Node.js, a JavaScript runtime system used to make server-side applications: 
+Background
+Dahl is an American software engineer born in 1981. He created Node.js in 2009, inspired by his criticism of Apache HTTP Server's limited ability to handle concurrent connections. 
+Features
+Node.js combines Google's V8 JavaScript engine, an event loop, and a low-level I/O API. The initial release only supported Linux and Mac OS X, but a Windows version was implemented in 2011. 
+Other work
+Dahl is also the creator of Deno, a JavaScript/TypeScript runtime. He also introduced JSR, an alternative to NPM. 
+Presentations
+Dahl has spoken about the creation of Node.js, including: 
+The importance of security in the JavaScript ecosystem 
+The future of JavaScript 
+Bridging the gap between server-side and browser JavaScript 
+Protecting against malicious use 
+The need for funding for open source projects
+*/
+console.log("hello world")
+console.log(33+3)
+console.log(33333+33)
+var a=3;
+console.log(a);
